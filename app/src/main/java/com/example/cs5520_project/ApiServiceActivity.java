e449db5920658cfe39f4fb28042a766e20127c43
Catalina <cgutierrezm95@gmail.com>
2022-10-20 15:40:00 -0400

    Added layout with input controls

---
+++ app/src/main/java/com/example/cs5520_project/ApiServiceActivity.java
@@ -0,0 +1,15 @@
+package com.example.cs5520_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApiServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_api_service);
+
+    }
+}
