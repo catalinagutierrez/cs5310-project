4c34993a047ead5e5229c18a8c35ff1b205fcb40
Dan Paddock <danpaddk@gmail.com>
2022-11-04 14:47:04 -0400

    small fix

--- app/src/main/java/com/example/cs5520_project/UserProfileActivity.java
+++ app/src/main/java/com/example/cs5520_project/UserProfileActivity.java
@@ -119,7 +119,6 @@ public class UserProfileActivity extends AppCompatActivity {
                             }
                         });
                     }
-                    myAdapter.notifyDataSetChanged();
                 }
             }
             @Override
